afd8ead978400a13f5becafa202e7c404ae0993c
Dr.BALAGANESH P <drbalaganesh.dentist@gmail.com>
2025-07-28 19:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,12 +117,10 @@ if uploaded_file:
         pdf.image(header_path, x=10, y=10, w=190)
         
         # Title Section
-        pdf.set_y(80)
+        pdf.set_y(100)
         pdf.set_font("Courier", style='B', size=16)
         pdf.cell(pdf.w - 2 * pdf.l_margin, 10, txt="IMPLANT SYSTEM DETECTION REPORT", ln=True, align='C')
         
-        pdf.set_font("Courier", style='B', size=14)
-        pdf.cell(pdf.w - 2 * pdf.l_margin, 10, txt="IMPLANT REPORT", ln=True, align='C')
 
 
         def add_each_implant(title, data):
