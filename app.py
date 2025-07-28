e2402810c90e0cc6eadeef446ddcff6732594827
Dr.BALAGANESH P <drbalaganesh.dentist@gmail.com>
2025-07-28 10:20:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if uploaded_file:
 # --------- CONTACT LINKS ----------
 st.markdown("""
 ### 📬 Contact
-<a href="mailto:drbalaganesh.dentist" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" width="30"></a>
+<a href="mailto:drbalaganesh.dentist" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" width="30"></a> + " " +
 <a href="https://github.com/balaganesh7601" target="_blank"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="30"></a>
 <a href="https://www.linkedin.com/in/drbalaganeshdentist/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="30"></a>
 <a href="https://www.instagram.com/_bala.7601/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" width="30"></a>
